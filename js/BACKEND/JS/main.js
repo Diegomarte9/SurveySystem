990ec15883ec362a1a951a79fa4747ed4d8eb106
Diegomarte9 <diegomarte9@gmail.com>
2024-10-29 15:57:54 -0400

    Cambios

---
+++ js/BACKEND/JS/main.js
@@ -0,0 +1,10 @@
+const loginBtn = document.querySelector("#login");
+const registerBtn = document.querySelector("#register");
+const loginForm = document.querySelector("#login-form");
+const registerForm = document.querySelector("#register-form");
+
+
+
+loginBtn.addEventListener('click', () => {
+    loginBtn.c
+})
